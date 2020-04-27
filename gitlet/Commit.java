cf061ae23c8206e1ce11cc2d10b26d1d8810c904
Christopher Seo <christophseo@gmail.com>
2020-04-27 16:05:50 -0700

    added fields for initial commits and merges

--- gitlet/Commit.java
+++ gitlet/Commit.java
@@ -1,6 +1,9 @@
 package gitlet;
 
+import java.io.File;
 import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 /** Class that contains file blobs, a commit message, references to parents,
@@ -10,13 +13,25 @@ import java.util.HashMap;
 public class Commit implements Serializable {
 
     static final String COMMIT_STR = "commit";
+    static final String INIT_DATE = "Wed Dec 31 16:00:00 1969 -0800";
+    static final File GITLET_DIR = Main.GITLET_DIR;
 
-    public Commit(String timestamp, String message, String parent,
-                  HashMap<String, Blob> blobs) {
-        _timestamp = timestamp;
+    public Commit(String message, String parent,
+                  HashMap<String, Blob> blobs, boolean isMerge,
+                                                boolean isInitial) {
+        if (isInitial) {
+            _timestamp = INIT_DATE;
+        } else {
+            String date = DateTimeFormatter.ofPattern("EEE " + "LLL " + "dd "
+                    + "HH:mm:ss " + "yyyy " + "Z").
+                    format(ZonedDateTime.now());
+            _timestamp = date;
+        }
+        _parent2 = null;
         _message = message;
         _parent = parent;
         _blobs = blobs;
+        _isMerge = isMerge;
         generateID();
     }
 
@@ -28,8 +43,28 @@ public class Commit implements Serializable {
         return _message;
     }
 
-    public String getParentID() {
-        return _parent;
+    public Commit getParent() {
+        if (_parent == null) {
+            return null;
+        }
+        File file = Utils.join(GITLET_DIR, _parent);
+        return Utils.readObject(file, Commit.class);
+    }
+
+    public Commit getParent2() {
+        if (_isMerge) {
+            File file = Utils.join(GITLET_DIR, _parent2);
+            return Utils.readObject(file, Commit.class);
+        }
+        return null;
+    }
+
+    public void setParent2ID(String parent2ID) {
+        _parent2 = parent2ID;
+    }
+
+    public boolean isMerge() {
+        return _isMerge;
     }
 
     public HashMap<String, Blob> getBlobs() {
@@ -46,6 +81,8 @@ public class Commit implements Serializable {
     }
 
     private String _parent;
+    private String _parent2;
+    private boolean _isMerge;
     private String _identifier;
     private String _timestamp;
     private String _message;
